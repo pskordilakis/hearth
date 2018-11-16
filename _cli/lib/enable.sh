76b3b2231e39a3ebcad770f161d9ba8e55e6bb0d
Panagiotis Skordilakis <p.skordilakis@gmail.com>
2018-11-16 20:02:47 +0200

    fix: enable secret recipe

--- _cli/lib/enable.sh
+++ _cli/lib/enable.sh
@@ -15,9 +15,8 @@ enable_recipe() {
     if [[ -f $HEARTH_HOME/${types[$1]}/available/$2 ]]; then
         ln -s "$HEARTH_HOME/${types[$1]}/available/$2" "$HEARTH_HOME/${types[$1]}/enabled"
     elif [[ -f $HEARTH_HOME/${types[$1]}/secret/$2 ]]; then
-        ln -s "$HEARTH_HOME/${types[$1]}/available/$2" "$HEARTH_HOME/${types[$1]}/enabled"
+        ln -s "$HEARTH_HOME/${types[$1]}/secret/$2" "$HEARTH_HOME/${types[$1]}/enabled"
     else
-        printf "recipe does not exist\n" 1>&2
-        exit 1
+        error "recipe $2 does not exist\n" 1>&2
     fi
 }
