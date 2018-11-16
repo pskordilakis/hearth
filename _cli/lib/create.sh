fad797b8d45eacf516427b364b9e6dc072598d49
Panagiotis Skordilakis <p.skordilakis@gmail.com>
2018-11-16 20:03:14 +0200

    add create-secret subcommand

--- _cli/lib/create.sh
+++ _cli/lib/create.sh
@@ -20,3 +20,22 @@ create_recipe() {
         touch $filename
     fi
 }
+
+#################################################
+# Create a secret recipe
+# Globals:
+#   types
+# Arguments:
+#   recipe
+# Returns:
+#   None
+#################################################
+create_secret_recipe() {
+    local filename="$HEARTH_HOME/${types[$1]}/secret/$2"
+
+    if [[ -n $EDITOR ]]; then
+        $EDITOR $filename
+    else
+        touch $filename
+    fi
+}
