c078712bd185a60307d830d56313894b17abe53d
Panagiotis Skordilakis <p.skordilakis@gmail.com>
2018-11-16 14:01:51 +0200

    error helper function

---
+++ _helpers/error.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+#
+# Error helper functions
+
+#################################################
+# Print error message and exit
+# Globals:
+#   None
+# Arguments:
+#   error
+# Returns:
+#   None
+#################################################
+error() {
+    printf "$1" 1>&2
+        exit 1
+}
