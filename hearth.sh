a50cbbd077ef4ad8ad2902b252b597f8bc0a0e63
Helias Fylactos <fylactosdev@gmail.com>
2018-11-17 15:22:29 +0100

    alternative syntacx to get the dirname and the script everywh3re

--- hearth.sh
+++ hearth.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/local/bin
 #
 # Entry point of Hearth
 # Here we will load all aliases, paths and functions
 
-export HEARTH_HOME=$(dirname $0)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+export HEARTH_HOME=$DIR
 
 source $HEARTH_HOME/_helpers/include.sh
 
@@ -18,3 +20,4 @@ include_symlinks $HEARTH_HOME/paths.d/enabled
 
 include_if_exists $HEARTH_HOME/functions.sh
 include_symlinks $HEARTH_HOME/functions.d/enabled
+echo "hearth loaded"
