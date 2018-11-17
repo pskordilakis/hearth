2f7102626802bf0c7c31073272ab9ae4c4f4a18e
Ilias Fylaktos <fylactosdev@gmail.com>
2018-11-17 15:49:44 +0100

    alternative syntax to get the dirname and the script everywhere (#3)
    * alternative syntacx to get the dirname and the script everywh3re
    
    * delete uneeded files and echos
    
    * runnning command in export line

--- hearth.sh
+++ hearth.sh
@@ -3,7 +3,7 @@
 # Entry point of Hearth
 # Here we will load all aliases, paths and functions
 
-export HEARTH_HOME=$(dirname $0)
+export HEARTH_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 source $HEARTH_HOME/_helpers/include.sh
 
