80a0fc1b6973ae0b539695bfa65107478fb586ad
Helias Fylactos <fylactosdev@gmail.com>
2018-11-17 15:48:45 +0100

    runnning command in export line

--- hearth.sh
+++ hearth.sh
@@ -3,9 +3,7 @@
 # Entry point of Hearth
 # Here we will load all aliases, paths and functions
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
-export HEARTH_HOME=$DIR
+export HEARTH_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 source $HEARTH_HOME/_helpers/include.sh
 
